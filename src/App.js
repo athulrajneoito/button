343d2cbb72b68d9fec072428eb4a62d4703e3ac2
athulrajneoito <athul.r@neoito.com>
2022-12-14 16:22:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Button from './button'
+export default function App() {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
---
+++ src/button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Button = () => {
+  return (
+   <p>Hello from button </p>
+  )
+}
+export default Button
